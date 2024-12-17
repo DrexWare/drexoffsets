66de1fe1a056ae1f2b1fc008e80e39aecf8996a4
Drexxy <patrick@savells.net>
2024-12-17 01:00:30 -0600

    Adding buttons.cs

---
+++ dumped/CATEGORY/cs/buttons.cs
@@ -0,0 +1,24 @@
+// Generated using https://github.com/a2x/cs2-dumper
+// 2024-12-17 06:59:25.694459800 UTC
+
+namespace CS2Dumper {
+    // Module: client.dll
+    public static class Buttons {
+        public const nint attack = 0x184E4D0;
+        public const nint attack2 = 0x184E560;
+        public const nint back = 0x184E7A0;
+        public const nint duck = 0x184EA70;
+        public const nint forward = 0x184E710;
+        public const nint jump = 0x184E9E0;
+        public const nint left = 0x184E830;
+        public const nint lookatweapon = 0x1A5E1A0;
+        public const nint reload = 0x184E440;
+        public const nint right = 0x184E8C0;
+        public const nint showscores = 0x1A5E080;
+        public const nint sprint = 0x184E3B0;
+        public const nint turnleft = 0x184E5F0;
+        public const nint turnright = 0x184E680;
+        public const nint use = 0x184E950;
+        public const nint zoom = 0x1A5E110;
+    }
+}
