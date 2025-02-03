90343eddf4224c086d66e5fc07e9d603283aae1e
Drex <patrick@savells.net>
2025-02-03 11:26:17 -0800

    Updating materialsystem2_dll.cs

--- dumped/cs/materialsystem2_dll.cs
+++ dumped/cs/materialsystem2_dll.cs
@@ -1,5 +1,5 @@
 // Generated using https://github.com/a2x/cs2-dumper
-// 2025-01-29 00:08:29.405864 UTC
+// 2025-02-03 19:16:49.420932 UTC
 
 namespace CS2Dumper.Schemas {
     // Module: materialsystem2.dll
@@ -175,7 +175,7 @@ namespace CS2Dumper.Schemas {
         // Metadata:
         // MGetKV3ClassDefaults
         public static class PostProcessingBloomParameters_t {
-            public const nint m_blendMode = 0x0; // BloomBlendMode_t
+            public const nint m_blendMode = 0x0; // 
             public const nint m_flBloomStrength = 0x4; // float32
             public const nint m_flScreenBloomStrength = 0x8; // float32
             public const nint m_flBlurBloomStrength = 0xC; // float32
