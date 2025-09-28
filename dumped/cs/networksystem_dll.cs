5b6febd6b658eb8ed1fa01ab8d3f2ccdade07110
Drex <patrick@savells.net>
2025-09-28 16:02:29 -0700

    Updating networksystem_dll.cs

--- dumped/cs/networksystem_dll.cs
+++ dumped/cs/networksystem_dll.cs
@@ -1,11 +1,18 @@
-// Generated using https://github.com/a2x/cs2-dumper
-// 2025-03-06 01:51:32.068275100 UTC
+// Dumped by shxdows dumper (csdump)
 
 namespace CS2Dumper.Schemas {
     // Module: networksystem.dll
     // Class count: 1
-    // Enum count: 0
+    // Enum count: 1
     public static class NetworksystemDll {
+        // Alignment: 4
+        // Member count: 4
+        public enum OutOfPVSUpdates_t : uint {
+            OOPVSUpdates_OptOut = 0x0,
+            OOPVSUpdates_OptIn = 0x1,
+            OOPVSUpdates_Default = 0x2,
+            OOPVSUpdates_Count = 0x3
+        }
         // Parent: None
         // Field count: 1
         public static class ChangeAccessorFieldPathIndex_t {
