541fb39bc11ccbadfc198418505c4181104af789
Drex <patrick@savells.net>
2025-09-28 16:15:29 -0700

    Adding networksystem_dll.py

---
+++ dumped/py/networksystem_dll.py
@@ -0,0 +1,14 @@
+// dumped by shxdows dumper (csdump)
+# dumped by shxdows dumper (csdump) 
+
+from enum import Enum
+
+class OutOfPVSUpdates_t(Enum):
+    OOPVSUpdates_Count = 3
+    OOPVSUpdates_Default = 2
+    OOPVSUpdates_OptIn = 1
+    OOPVSUpdates_OptOut = 0
+
+class ChangeAccessorFieldPathIndex_t:
+    m_Value = 0  # offset
+
