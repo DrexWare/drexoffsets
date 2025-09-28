d367120239f14cc3d18c4789e9d2ad86b0fdc848
Drex <patrick@savells.net>
2025-09-28 16:15:47 -0700

    Adding steamaudio_dll.py

---
+++ dumped/py/steamaudio_dll.py
@@ -0,0 +1,192 @@
+// dumped by shxdows dumper (csdump)
+# dumped by shxdows dumper (csdump) 
+
+class CSteamAudioAmbisonicsField:
+    m_field = 0  # offset
+
+    __metadata__ =     [
+        {
+            "name": "MGetKV3ClassDefaults",
+            "type": "Unknown"
+        }
+    ]
+
+class CSteamAudioBakedDimensionsData:
+    m_probes = 24  # offset
+    m_settings = 0  # offset
+    m_vecInOut = 32  # offset
+    m_vecInsideSmallSizeField = 104  # offset
+    m_vecOutsideField = 80  # offset
+    m_vecSize = 56  # offset
+
+    __metadata__ =     [
+        {
+            "name": "MGetKV3ClassDefaults",
+            "type": "Unknown"
+        }
+    ]
+
+class CSteamAudioBakedMaterialsData:
+    m_probes = 0  # offset
+    m_vecMaterialTokens = 8  # offset
+    m_vecMaterialWeights = 32  # offset
+
+    __metadata__ =     [
+        {
+            "name": "MGetKV3ClassDefaults",
+            "type": "Unknown"
+        }
+    ]
+
+class CSteamAudioBakedOcclusionData:
+    m_probes = 16  # offset
+    m_settings = 0  # offset
+    m_vecPathingDeviation = 48  # offset
+    m_vecPathingRatio = 24  # offset
+    m_vecReflectionRatio = 72  # offset
+
+    __metadata__ =     [
+        {
+            "name": "MGetKV3ClassDefaults",
+            "type": "Unknown"
+        }
+    ]
+
+class CSteamAudioBakedPathingData:
+    m_nBands = 0  # offset
+    m_probes = 8  # offset
+
+    __metadata__ =     [
+        {
+            "name": "MGetKV3ClassDefaults",
+            "type": "Unknown"
+        }
+    ]
+
+class CSteamAudioBakedReverbData:
+    m_clusteredProbes = 48  # offset
+    m_compressedClusteredData = 176  # offset
+    m_compressedData = 80  # offset
+    m_nBands = 0  # offset
+    m_probes = 8  # offset
+    m_reverbClusteringSettings = 32  # offset
+    m_reverbCompressionSettings = 44  # offset
+    m_reverbSettings = 16  # offset
+    m_vecClusterForProbe = 56  # offset
+    m_vecMovableReverbAABBs = 320  # offset
+    m_vecMovableReverbInitialTransforms = 296  # offset
+    m_vecMovableReverbKeyTokens = 368  # offset
+    m_vecMovableReverbKeys = 344  # offset
+    m_vecMovableReverbOBBs = 416  # offset
+    m_vecMovableReverbTransforms = 392  # offset
+    m_vecMovableReverbs = 272  # offset
+
+    __metadata__ =     [
+        {
+            "name": "MGetKV3ClassDefaults",
+            "type": "Unknown"
+        }
+    ]
+
+class CSteamAudioCompressedReverb:
+    m_nBands = 4  # offset
+    m_nBins = 8  # offset
+    m_nChannels = 0  # offset
+    m_nProbes = 12  # offset
+    m_pCompressedData = 88  # offset
+    m_vecCompressedData = 64  # offset
+    m_vecDictionary = 40  # offset
+    m_vecNumSingularValues = 16  # offset
+
+    __metadata__ =     [
+        {
+            "name": "MGetKV3ClassDefaults",
+            "type": "Unknown"
+        }
+    ]
+
+class CSteamAudioProbeData:
+    m_pProbeBatch = 0  # offset
+
+    __metadata__ =     [
+        {
+            "name": "MGetKV3ClassDefaults",
+            "type": "Unknown"
+        }
+    ]
+
+class SteamAudioCustomDataDimensionsSettings_t:
+    m_flInsideThreshold = 16  # offset
+    m_flOutsideThreshold = 8  # offset
+    m_flSizeThreshold = 12  # offset
+    m_nAmbisonicsOrderInsideSizeField = 4  # offset
+    m_nAmbisonicsOrderOutsideField = 0  # offset
+
+    __metadata__ =     [
+        {
+            "name": "MGetKV3ClassDefaults",
+            "type": "Unknown"
+        }
+    ]
+
+class SteamAudioCustomDataOcclusionSettings_t:
+    m_bEnablePathing = 0  # offset
+    m_bEnableReflections = 1  # offset
+    m_nReflectionBounces = 8  # offset
+    m_nReflectionRays = 4  # offset
+
+    __metadata__ =     [
+        {
+            "name": "MGetKV3ClassDefaults",
+            "type": "Unknown"
+        }
+    ]
+
+class SteamAudioPathSettings_t:
+    m_flProbePathRange = 12  # offset
+    m_flProbeVisRadius = 4  # offset
+    m_flProbeVisThreshold = 8  # offset
+    m_nNumVisSamples = 0  # offset
+
+    __metadata__ =     [
+        {
+            "name": "MGetKV3ClassDefaults",
+            "type": "Unknown"
+        }
+    ]
+
+class SteamAudioReverbClusteringSettings_t:
+    m_bEnableClustering = 0  # offset
+    m_flDepthThreshold = 8  # offset
+    m_nCubeMapResolution = 4  # offset
+
+    __metadata__ =     [
+        {
+            "name": "MGetKV3ClassDefaults",
+            "type": "Unknown"
+        }
+    ]
+
+class SteamAudioReverbCompressionSettings_t:
+    m_bEnableCompression = 0  # offset
+
+    __metadata__ =     [
+        {
+            "name": "MGetKV3ClassDefaults",
+            "type": "Unknown"
+        }
+    ]
+
+class SteamAudioReverbSettings_t:
+    m_flIRDuration = 8  # offset
+    m_nAmbisonicsOrder = 12  # offset
+    m_nNumBounces = 4  # offset
+    m_nNumRays = 0  # offset
+
+    __metadata__ =     [
+        {
+            "name": "MGetKV3ClassDefaults",
+            "type": "Unknown"
+        }
+    ]
+
