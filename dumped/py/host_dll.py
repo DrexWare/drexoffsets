05a6b410eba5d4620120aae90047ca5f906e334d
Drex <patrick@savells.net>
2025-09-28 16:15:23 -0700

    Adding host_dll.py

---
+++ dumped/py/host_dll.py
@@ -0,0 +1,9 @@
+// dumped by shxdows dumper (csdump)
+# dumped by shxdows dumper (csdump) 
+
+class CAnimScriptBase:
+    m_bIsValid = 8  # offset
+
+class EmptyTestScript:
+    m_hTest = 16  # offset
+
