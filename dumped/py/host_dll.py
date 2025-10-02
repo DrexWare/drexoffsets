69ea4f1582643fb7e2d6b2c78605dd6c2ce2b4c1
c l o u d <patrick@savells.net>
2025-10-01 21:24:29 -0500

    Updating host_dll.py

--- dumped/py/host_dll.py
+++ dumped/py/host_dll.py
@@ -1,9 +1,7 @@
-// dumped by shxdows dumper (csdump)
-# dumped by shxdows dumper (csdump) 
+# dumped by shxdows dumper (csdump)
 
 class CAnimScriptBase:
     m_bIsValid = 8  # offset
 
 class EmptyTestScript:
     m_hTest = 16  # offset
-
