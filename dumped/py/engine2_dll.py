1f7258a7ce3b83692daf5cfb65a713f57250c78b
c l o u d <patrick@savells.net>
2025-10-01 21:24:28 -0500

    Updating engine2_dll.py

--- dumped/py/engine2_dll.py
+++ dumped/py/engine2_dll.py
@@ -1,5 +1,4 @@
-// dumped by shxdows dumper (csdump)
-# dumped by shxdows dumper (csdump) 
+# dumped by shxdows dumper (csdump)
 
 from enum import Enum
 
@@ -208,4 +207,3 @@ class GameTick_t:
 
 class GameTime_t:
     m_Value = 0  # offset
-
