092309a5c0883ede92adff82865effb597e45ccb
c l o u d <patrick@savells.net>
2025-10-01 21:24:35 -0500

    Updating worldrenderer_dll.py

--- dumped/py/worldrenderer_dll.py
+++ dumped/py/worldrenderer_dll.py
@@ -1,5 +1,4 @@
-// dumped by shxdows dumper (csdump)
-# dumped by shxdows dumper (csdump) 
+# dumped by shxdows dumper (csdump)
 
 from enum import Enum
 
@@ -387,4 +386,3 @@ class World_t:
             "type": "Unknown"
         }
     ]
-
