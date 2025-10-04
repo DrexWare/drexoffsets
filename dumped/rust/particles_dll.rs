28b423cd96839a95a1f91e91cd643dfd6f67ef54
c l o u d <patrick@savells.net>
2025-10-04 17:18:52 -0500

    Updating particles_dll.rs

--- dumped/rust/particles_dll.rs
+++ dumped/rust/particles_dll.rs
@@ -1,5 +1,5 @@
-// Generated using https://github.com/a2x/cs2-dumper
-// 2025-10-04 22:16:28.065591100 UTC
+// dumped with csdumper (created by shxdow)
+// 2025-10-04 22:18:51 UTC
 
 #![allow(non_upper_case_globals, non_camel_case_types, non_snake_case, unused)]
 
