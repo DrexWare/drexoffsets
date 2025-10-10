3be4d694e1bdee8d84557a9e9728815789ff0439
c l o u d <patrick@savells.net>
2025-10-10 10:35:37 -0500

    Updating offsets.hpp

--- dumped/c++/offsets.hpp
+++ dumped/c++/offsets.hpp
@@ -1,5 +1,5 @@
 // dumped with csdumper (created by shxdow)
-// 2025-10-04 22:19:09 UTC
+// 2025-10-10 15:35:36 UTC
 
 #pragma once
 
@@ -38,8 +38,8 @@ namespace cs2_dumper {
             constexpr std::ptrdiff_t dwNetworkGameClient_maxClients = 0x230;
             constexpr std::ptrdiff_t dwNetworkGameClient_serverTickCount = 0x23C;
             constexpr std::ptrdiff_t dwNetworkGameClient_signOnState = 0x220;
-            constexpr std::ptrdiff_t dwWindowHeight = 0x8EA624;
-            constexpr std::ptrdiff_t dwWindowWidth = 0x8EA620;
+            constexpr std::ptrdiff_t dwWindowHeight = 0x8EA63C;
+            constexpr std::ptrdiff_t dwWindowWidth = 0x8EA638;
         }
         // Module: inputsystem.dll
         namespace inputsystem_dll {
