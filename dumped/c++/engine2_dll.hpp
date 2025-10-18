7ffdce7729f14cf56ca2564cd1631cea549bce0d
c l o u d <patrick@savells.net>
2025-10-18 11:18:03 -0500

    Updating engine2_dll.hpp

--- dumped/c++/engine2_dll.hpp
+++ dumped/c++/engine2_dll.hpp
@@ -1,5 +1,5 @@
 // dumped with csdumper (created by shxdow)
-// 2025-10-14 02:09:25 UTC
+// 2025-10-18 16:18:01 UTC
 
 #pragma once
 
@@ -45,7 +45,7 @@ namespace cs2_dumper {
                 constexpr std::ptrdiff_t m_nTotalTicksThisFrame = 0x38; // int32
                 constexpr std::ptrdiff_t m_nTotalTicks = 0x3C; // int32
             }
-            // Parent: None
+            // Parent: 
             // Field count: 1
             namespace CEntityIOOutput {
                 constexpr std::ptrdiff_t m_Value = 0x18; // CVariantBase<CVariantDefaultAllocator>
